03463589d6e1774e2d5071ca4650f99dda810aa8
Benoit Lemaire <blemair@l-sb8h2ajg8w-m.homeoffice.wal-mart.com>
2017-08-03 18:50:30 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {
+  Component
+} from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import {
+  ColorPicker
+} from 'react-native-color-picker'
+import {
+  ColorPickerApi
+} from 'ern-color-picker-api'
+
+let currentColor
+
+export default class ColorPickerDemo extends Component {
+  alertOn = false //Useful to set to true when running standalone.
+  render() {
+    return ( <
+      ColorPicker onColorSelected = {
+        color => {
+          currentColor = color
+          if (this.alertOn) {
+            alert(`Color selected: ${currentColor}`)
+          }
+          ColorPickerApi.events().emitColorSelected(currentColor)
+        }
+      }
+      style = {
+        styles.container
+      }
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+ColorPickerApi.requests().registerGetSelectedColorRequestHandler((requestData) => {
+  if (currentColor) {
+    Promise.resolve(currentColor)
+  } else {
+    Promise.reject('No color currently selected')
+  }
+})
---
+++ index.android.js
@@ -0,0 +1,7 @@
+import {
+  AppRegistry
+} from 'react-native';
+import ColorPickerDemo from './App';
+
+
+AppRegistry.registerComponent('colorpickerdemominiapp', () => ColorPickerDemo);
